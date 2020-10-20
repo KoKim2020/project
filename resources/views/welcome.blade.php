fc0b7d3c8f669c53379c5c53e90d62f26004833e
KoKim <waiyanavionics@gmail.com>
2020-10-20 14:38:18 +0630

    sasZ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Testing 123  148654646469
                 </div>
 
                 <div class="links">
