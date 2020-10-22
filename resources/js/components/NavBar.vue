15f6b25b9856260e0b4220ff41aa595932374ac5
KoKim <waiyanavionics@gmail.com>
2020-10-22 23:33:46 +0630

    layout start

---
+++ resources/js/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="q-pa-md q-gutter-y-sm">
+    <div class="bg-orange text-white">
+      <q-toolbar>
+        <q-btn flat round dense icon="menu" class="q-mr-sm" />
+        <q-space />
+        <q-btn flat round dense icon="search" class="q-mr-xs" />
+        <q-btn flat round dense icon="group_add" />
+      </q-toolbar>
+      <q-toolbar inset>
+        <q-toolbar-title><strong>OakHouse</strong> Tehcnology</q-toolbar-title>
+      </q-toolbar>
+    </div>
+  </div>
+</template>
