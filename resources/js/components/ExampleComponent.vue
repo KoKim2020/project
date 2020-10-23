3715f1f6751dad16f6308af90c0559832bef7988
KoKim <waiyanavionics@gmail.com>
2020-10-24 03:09:41 +0630

    dd

--- resources/js/components/ExampleComponent.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <div class="w-full mx-auto bg-gray-400 h-48 bg-center bg-cover" style="background-image:url('https://images.unsplash.com/photo-1459262838948-3e2de6c1ec80?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80');">
-    </div>
-</template>
