6c9537e44697b85afdd760dda19a2ee5d2b0b0b3
KoKim <waiyanavionics@gmail.com>
2020-10-24 13:50:56 +0630

    fix responsive

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -1,16 +1,27 @@
 <template>
-  <div class="">
-    <div class="row no-wrap shadow-1">
-      <q-toolbar class="col-8 bg-grey-3">
-        <q-btn flat round dense icon="menu" />
-        <q-toolbar-title class="font-sans">OakHouse Technology</q-toolbar-title>
-        <q-btn flat round dense icon="search" />
-      </q-toolbar>
-      <q-toolbar class="col-4 text-white bg-grey-3">
-        <q-space />
-        <!-- <q-btn flat round dense icon="bluetooth" class="q-mr-sm" /> -->
-        <q-btn flat round dense icon="more_vert" />
-      </q-toolbar>
-    </div>
-  </div>
+	<div class="">
+		<div class="row no-wrap shadow-1">
+			<q-toolbar class="col-8 bg-grey-3">
+				<q-btn flat round dense icon="menu" />
+				<q-toolbar-title class="font-sans">OakHouse Technology</q-toolbar-title>
+				<q-btn flat round dense icon="search" />
+			</q-toolbar>
+			<q-toolbar class="col-4 text-white bg-grey-3">
+				<q-space />
+				<!-- <q-btn flat round dense icon="bluetooth" class="q-mr-sm" /> -->
+				<q-btn flat round dense icon="more_vert" />
+			</q-toolbar>
+		</div>
+	</div>
 </template>
+
+<style >
+	.q-toolbar__title {
+		font-size: 15px !important;
+	}
+	@media (min-width: 640px) {
+		.q-toolbar__title {
+			font-size: 21px !important;
+		}
+	}
+</style>
--- resources/views/background_image.blade.php
+++ resources/views/background_image.blade.php
@@ -1,12 +1,28 @@
-<div class="w-full h-64 md:h-screen  bg-center bg-cover" style="background-image:url('{{ asset('images/tesla_bg.jpg') }}');">
-    <div class="md:flex md:justify-center">
-        <div class="pt-5 md:pt-20 px-5 md:w-1/2 w-full">
-            <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-1 md:py-3 shadow-md" role="alert">
-                <div class="flex">
-                    <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+<div class="tw-w-full tw-hidden md:tw-block tw-h-screen  tw-bg-center tw-bg-cover" style="background-image:url('{{ asset('images/tesla_bg.jpg') }}');">
+    <div class="md:tw-flex md:tw-justify-center">
+        <div class="tw-pt-5 md:tw-pt-20 tw-px-5 md:tw-w-1/2 tw-w-full">
+            <div class="tw-bg-teal-100 tw-border-t-4 tw-border-teal-500 tw-rounded-b tw-text-teal-900 tw-px-4 tw-py-1 md:tw-py-3 tw-shadow-md" role="alert">
+                <div class="tw-flex">
+                    <div class="tw-py-1"><svg class="tw-fill-current tw-h-6 tw-w-6 tw-text-teal-500 tw-mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
                     <div>
-                        <p class="font-bold font-sans">Hello Friend</p>
-                        <p class="text-sm font-sans">Welcome From OakHouse Technology.</p>
+                        <p class="tw-font-bold tw-font-sans">Hello Friend</p>
+                        <p class="tw-text-sm tw-font-sans">Welcome From OakHouse Technology.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="md:tw-hidden tw-block">
+    <div class="tw-flex tw-justify-center">
+        <div class="tw-pt-5 md:tw-pt-20 tw-px-5 md:tw-w-1/2 tw-w-full">
+            <div class="tw-bg-teal-100 tw-border-t-4 tw-border-teal-500 tw-rounded-b tw-text-teal-900 tw-px-4 tw-py-1 md:tw-py-3 tw-shadow-md" role="alert">
+                <div class="tw-flex">
+                    <div class="tw-py-1"><svg class="tw-fill-current tw-h-6 tw-w-6 tw-text-teal-500 tw-mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+                    <div>
+                        <p class="tw-font-bold tw-font-sans">Hello Friend</p>
+                        <p class="tw-text-sm tw-font-sans">Welcome From OakHouse Technology.</p>
                     </div>
                 </div>
             </div>
