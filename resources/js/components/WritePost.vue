4fb39bc2039027ed29b1c78f022d5c4c5d3990ee
KoKim <waiyanavionics@gmail.com>
2020-11-02 21:40:38 +0630

    text area added

--- resources/js/components/WritePost.vue
+++ resources/js/components/WritePost.vue
@@ -1,29 +1,77 @@
 <template>
     <div class="q-pa-sm">
-        <q-card class="my-card">
-            <q-item>
-                <q-item-section avatar>
-                    <q-avatar>
-                        <img src="https://cdn.quasar.dev/img/avatar2.jpg">
-                    </q-avatar>
-                </q-item-section>
+        <q-card class="my-card tw-cursor-pointer" @click="writePost = true">
+            <q-card-section>
+                <q-item>
+                    <q-item-section avatar>
+                        <q-avatar>
+                            <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+                        </q-avatar>
+                    </q-item-section>
 
-                <q-item-section>
-                    <q-item-label>Kyaw Kyaw</q-item-label>
-                    <q-item-label caption>
-                        <q-icon name="create" />
-                        Start A Post
-                    </q-item-label>
-                </q-item-section>
-            </q-item>
+                    <q-item-section>
+                        <q-item-label>Kyaw Kyaw</q-item-label>
+                        <q-item-label caption>
+                            <q-icon name="create" />
+                            Start A Post
+                        </q-item-label>
+                    </q-item-section>
+                </q-item>
+            </q-card-section>
         </q-card>
+
+        <q-dialog v-model="writePost" style="width: 700px; max-width: 80vw;">
+            <q-card>
+                <q-card-section>
+                    <div class="text-h6">Create a Post</div>
+                </q-card-section>
+                <q-separator />
+                <!-- writer name and image -->
+                <q-card-section class="q-pa-none">
+                    <q-item>
+                        <q-item-section avatar>
+                            <q-avatar>
+                                <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+                            </q-avatar>
+                        </q-item-section>
+
+                        <q-item-section >
+                            <q-item-label class="tw-font-bold tw-pb-3">Kyaw Kyaw</q-item-label>
+                        </q-item-section>
+                    </q-item>
+                </q-card-section>
+                
+                <q-card-section class="">
+                    <div class="" style="width: 500px">
+                        <q-input
+                            v-model="text"
+                            filled
+                            type="textarea"
+                            label="What Do You Want To Share About?"
+                        />
+                    </div>
+                </q-card-section>
+
+                <q-card-actions align="right">
+                    <q-btn flat label="OK" color="primary" v-close-popup />
+                </q-card-actions>
+            </q-card>
+        </q-dialog>
     </div>
 </template>
-
-
 <style>
     .my-card {
         width: 100%;
         max-width: 450px
     }
 </style>
+
+<script>
+export default {
+    data () {
+        return {
+            writePost: false
+        }
+    }
+}
+</script>
