590b3eecf74e8d5d1d1c22720232bc0b7bfaa661
KoKim <waiyanavionics@gmail.com>
2020-11-02 10:49:19 +0630

    modify verify blade

---
+++ resources/js/components/VerifyDialog.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <q-dialog v-model="alert" persistent>
+            <q-card>
+                <q-card-section>
+                    <div class="text-h6">{{ title }}</div>
+                </q-card-section>
+
+                <q-card-section class="q-pt-none">
+                    <slot></slot>
+                </q-card-section>
+
+                <q-card-actions align="right">
+                </q-card-actions>
+            </q-card>
+        </q-dialog>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['title'],
+    data () {
+        return {
+            alert: true,
+        }
+    }
+}
+</script>
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,28 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
+    <verify-dialog title="{{ __('Verify Your Email Address') }}">
+        <div>
+            @if (session('resent'))
+                <div class="">
+                    <p class="tw-text-sm">
+                        {{ __('A fresh verification link has been sent to your email address.') }}
+                    </p>
                 </div>
+            @endif
+            <div class="">
+                {{ __('Before proceeding, please check your email for a verification link.') }}
+                {{ __('If you did not receive the email') }},
+                <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                    @csrf
+                    <q-btn flat color="primary" type="submit" no-caps>
+                        <div>{{ __('click here to request another') }}</div>
+                    </q-btn>
+                </form>
             </div>
         </div>
-    </div>
-</div>
+    </verify-dialog>
 @endsection
