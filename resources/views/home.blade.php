4541a6b406955d2564518e11a1766ed99f6f8ac2
KoKim <waiyanavionics@gmail.com>
2020-11-07 23:05:34 +0630

    mobile responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
     <div class="tw-container tw-mx-auto tw-px-4">
-        <div class="tw-flex tw-p-12">
+        <div class="tw-flex md:tw-p-12">
             {{-- bussiness --}}
-            <div class="tw-w-1/4">
+            <div class="tw-hidden md:tw-block md:tw-w-1/4">
                 <business-list></business-list>
             </div>
             {{-- news feed --}}
-            <div class="tw-w-2/4">
+            <div class="tw-w-full md:tw-w-2/4">
                 <div class="tw-flex tw-justify-center">
                     <div class="">
                         <write-post></write-post>
@@ -17,7 +17,7 @@
                 </div>
             </div>
             {{-- advertisements --}}
-            <div class="tw-w-1/4">
+            <div class="tw-hidden md:tw-block tw-w-1/4">
                 <advertisement-list></advertisement-list>
             </div>
         </div>
