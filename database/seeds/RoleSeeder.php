5fd0e818fb98a2632315d6217831aadd7e276fb3
KoKim <waiyanavionics@gmail.com>
2020-11-07 13:25:07 +0630

    role seeder

---
+++ database/seeds/RoleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            'name' => 'admin',
+            'display_name' => 'Administrator'
+        ]);
+
+        DB::table('roles')->insert([
+            'name' => 'user',
+            'display_name' => 'Normal User'
+        ]);
+    }
+}
