96c6172eb7be643ab74be0956729993382622ebf
KoKim <waiyanavionics@gmail.com>
2020-11-08 18:32:29 +0630

    fix responsive

---
+++ resources/views/home_mobile.blade.php
@@ -0,0 +1,8 @@
+<div class="tw-container tw-mx-auto tw-px-4">
+    <div class="tw-flex tw-justify-center">
+        <div class="">
+            <write-post></write-post>
+            <news-feed :posts="{{ $posts }}"></news-feed>
+        </div>
+    </div>
+</div>
---
+++ resources/views/home_pc.blade.php
@@ -0,0 +1,21 @@
+<div class="tw-container tw-mx-auto tw-px-4">
+    <div class="tw-flex tw-p-12">
+        {{-- bussiness --}}
+        <div class="tw-w-1/4">
+            <business-list></business-list>
+        </div>
+        {{-- news feed --}}
+        <div class="tw-w-2/4">
+            <div class="tw-flex tw-justify-center">
+                <div class="">
+                    <write-post></write-post>
+                    <news-feed :posts="{{ $posts }}"></news-feed>
+                </div>
+            </div>
+        </div>
+        {{-- advertisements --}}
+        <div class="tw-w-1/4">
+            <advertisement-list></advertisement-list>
+        </div>
+    </div>
+</div>
