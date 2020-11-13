5b90b2c79f605e7d4f14460bab403df9b87e8549
KoKim <waiyanavionics@gmail.com>
2020-11-13 13:24:53 +0630

    use blade template

--- resources/views/post_detail.blade.php
+++ resources/views/post_detail.blade.php
@@ -3,7 +3,20 @@
 @section('content')
     <div class="tw-container tw-mx-auto tw-px-4">
         <div class="tw-flex tw-justify-center">
-            <post-detail :post="{{ $post }}"></post-detail>
+            <div class="tw-max-w-sm tw-rounded tw-overflow-hidden tw-shadow-lg" style="max-width: 750px; width: 100%; padding: 25px;">
+                <img class="tw-w-full" src="{{ $post->img_url }}" alt="Sunset in the mountains">
+                    <div class="q-pa-md">
+                        <div class="tw-font-bold tw-text-xl tw-mb-2">{{ $post->title }}</div>
+                        <p class="tw-text-gray-700 tw-text-base">
+                            {!! $post->body !!}
+                        </p>
+                    </div>
+                <div class="tw-px-6 tw-pt-4 tw-pb-2">
+                    <span class="tw-inline-block tw-bg-gray-200 tw-rounded-full tw-px-3 tw-py-1 tw-text-sm tw-font-semibold tw-text-gray-700 tw-mr-2 tw-mb-2">#photography</span>
+                    <span class="tw-inline-block tw-bg-gray-200 tw-rounded-full tw-px-3 tw-py-1 tw-text-sm tw-font-semibold tw-text-gray-700 tw-mr-2 tw-mb-2">#travel</span>
+                    <span class="tw-inline-block tw-bg-gray-200 tw-rounded-full tw-px-3 tw-py-1 tw-text-sm tw-font-semibold tw-text-gray-700 tw-mr-2 tw-mb-2">#winter</span>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
