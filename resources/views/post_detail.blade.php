2817af1ae1a51a5e0600c76d64be522e0109e4c2
KoKim <waiyanavionics@gmail.com>
2020-11-13 12:42:44 +0630

    s

--- resources/views/post_detail.blade.php
+++ resources/views/post_detail.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     <div class="tw-container tw-mx-auto tw-px-4">
         <div class="tw-flex tw-justify-center">
-            <post-detail :post="{{ $post }}"></post-detail>
+            <img src="{{ $post->img_url }}" alt="">
+            {{-- <post-detail :post="{{ $post }}"></post-detail> --}}
         </div>
     </div>
 @endsection
