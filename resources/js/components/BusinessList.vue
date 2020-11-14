62db2ce63b3450b8123e57bb9491714a8047da56
KoKim <waiyanavionics@gmail.com>
2020-11-14 11:29:07 +0630

    fix business

--- resources/js/components/BusinessList.vue
+++ resources/js/components/BusinessList.vue
@@ -6,71 +6,18 @@
                     <q-item-label class="text-no-wrap">OAKHOUSE ELECTRONICS STORE</q-item-label>
                     <q-item-label class="tw-text-red-700">One Sale Now</q-item-label>
                     <q-item-label caption>
-                        You Can Buy The following Components in OAKHOUSE-TECH
+                        You Can Buy The following Products in OAKHOUSE-TECH
                     </q-item-label>
                 </q-item-section>
             </q-item>
-            <q-item clickable v-ripple>
-                <q-item-section avatar>
-                    <q-icon color="primary" name="bluetooth" />
-                </q-item-section>
-
-                <q-item-section>Icon as avatar</q-item-section>
-            </q-item>
-
-            <q-item clickable v-ripple>
-                <q-item-section avatar>
-                    <q-avatar color="teal" text-color="white" icon="bluetooth" />
-                </q-item-section>
-
-                <q-item-section>Avatar-type icon</q-item-section>
-            </q-item>
-
-            <q-item clickable v-ripple>
-                <q-item-section avatar>
-                    <q-avatar rounded color="purple" text-color="white" icon="bluetooth" />
-                </q-item-section>
-
-                <q-item-section>Rounded avatar-type icon</q-item-section>
-            </q-item>
-
-            <q-item clickable v-ripple>
-                <q-item-section avatar>
-                    <q-avatar color="primary" text-color="white">
-                        R
-                    </q-avatar>
-                </q-item-section>
-
-                <q-item-section>Letter avatar-type</q-item-section>
-            </q-item>
-
-            <q-separator />
-
-            <q-item clickable v-ripple>
-                <q-item-section avatar>
-                    <q-avatar>
-                        <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-                    </q-avatar>
-                </q-item-section>
-                <q-item-section>Image avatar</q-item-section>
-            </q-item>
-
-            <q-item clickable v-ripple>
-                <q-item-section avatar>
-                    <q-avatar square>
-                        <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-                    </q-avatar>
-                </q-item-section>
-                <q-item-section>Image square avatar</q-item-section>
-            </q-item>
-
+    
             <q-item clickable v-ripple>
                 <q-item-section avatar>
                     <q-avatar rounded>
-                        <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+                        <img src="https://img.icons8.com/color/50/000000/wifi-logo.png"/>                
                     </q-avatar>
                 </q-item-section>
-                <q-item-section>Image rounded avatar</q-item-section>
+                <q-item-section>Wireless Sensors</q-item-section>
             </q-item>
 
             <q-separator />
@@ -78,17 +25,76 @@
             <q-item clickable v-ripple>
                 <q-item-section avatar>
                     <q-avatar rounded>
-                        <img src="https://cdn.quasar.dev/img/mountains.jpg">
+                        <img src="https://img.icons8.com/dusk/64/000000/resistor.png"/>                    
                     </q-avatar>
                 </q-item-section>
-                <q-item-section>List item</q-item-section>
+                <q-item-section>Resistors, Capacitors, And Electronics Components</q-item-section>
             </q-item>
 
             <q-item clickable v-ripple>
-                <q-item-section thumbnail>
-                    <img src="https://cdn.quasar.dev/img/mountains.jpg">
+                <q-item-section avatar>
+                    <q-avatar rounded>
+                        <img src="https://img.icons8.com/ultraviolet/40/000000/stepper-motor.png"/>                    
+                    </q-avatar>
                 </q-item-section>
-                <q-item-section>List item</q-item-section>
+                <q-item-section>Stepper Motors & DC Motors</q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+                <q-item-section avatar>
+                    <q-avatar rounded>
+                        <img src="https://img.icons8.com/metro/26/000000/integrated-circuit.png"/>
+                    </q-avatar>
+                </q-item-section>
+                <q-item-section>Motor Drivers</q-item-section>
+            </q-item>
+
+            <q-separator />
+
+            <q-item clickable v-ripple>
+                <q-item-section avatar>
+                    <q-avatar rounded>
+                        <img src="https://img.icons8.com/dotty/80/000000/arduino-uno-board.png"/>                
+                    </q-avatar>
+                </q-item-section>
+                <q-item-section>Arduino Boards</q-item-section>
+            </q-item>
+            <q-item clickable v-ripple>
+                <q-item-section avatar>
+                    <q-avatar rounded>
+                        <img src="https://img.icons8.com/color/48/000000/raspberry-pi-zero.png"/>
+                    </q-avatar>
+                </q-item-section>
+                <q-item-section>Respberry Pi Boards</q-item-section>
+            </q-item>
+
+            <q-separator />
+
+            <q-item clickable v-ripple>
+                <q-item-section avatar>
+                    <q-avatar rounded>
+                        <img src="https://img.icons8.com/dusk/64/000000/medium-battery.png"/>                    
+                    </q-avatar>
+                </q-item-section>
+                <q-item-section>Batteries</q-item-section>
+            </q-item>
+            <q-separator />
+
+            <q-item clickable v-ripple>
+                <q-item-section avatar>
+                    <q-avatar rounded>
+                        <img src="https://img.icons8.com/bubbles/50/000000/airplane-take-off.png"/>
+                    </q-avatar>
+                </q-item-section>
+                <q-item-section>RC Airplanes</q-item-section>
+            </q-item>
+            <q-item clickable v-ripple>
+                <q-item-section avatar>
+                    <q-avatar rounded>
+                        <img src="https://img.icons8.com/color/48/000000/f1-race-car-top-veiw.png"/>                    
+                    </q-avatar>
+                </q-item-section>
+                <q-item-section>RC Cars</q-item-section>
             </q-item>
         </q-list>
     </div>
