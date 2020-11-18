41b9807b38c28f646eddf457b7453304cba20c31
KoKim <waiyanavionics@gmail.com>
2020-11-18 09:57:51 +0630

    image handler added

--- resources/js/components/AddPost.vue
+++ resources/js/components/AddPost.vue
@@ -18,11 +18,12 @@
                         @blur="onEditorBlur($event)"
                         @focus="onEditorFocus($event)"
                         @ready="onEditorReady($event)"
+                        rows="20"
                     />
                 </q-card-section>
 
                 <q-card-actions align="right">
-                    <q-btn flat label="test" color="primary" @click="test" />
+                    <!-- <q-btn flat label="test" color="primary" @click="test" /> -->
                     <q-btn flat label="OK" color="primary" v-close-popup />
                 </q-card-actions>
             </q-card>
@@ -31,13 +32,45 @@
 </template>
 
 <script>
+    var toolbarOptions = [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
+        ['link', 'image'],
 
+        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+        [{ 'direction': 'rtl' }],                         // text direction
+
+        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+
+        ['clean']                                         // remove formatting button
+    ];
     export default {
         data () {
             return {
                 alert: false,
                 content: '<p>Write a Post Here.</p',
                 editorOption: {
+                    modules: {
+                        toolbar: {
+                            container: toolbarOptions,
+                            handlers: {
+                                image: function(value) {
+                                    console.log(value)
+                                }
+                            }
+                        },
+                        imageResize: {
+                            modules: ["Resize", "DisplaySize", "Toolbar"]
+                        }
+                    }
                 }
             }
         },
@@ -54,9 +87,6 @@
             onEditorChange({ quill, html, text }) {
                 console.log('editor change!', quill, html, text)
                 this.content = html
-            },
-            test () {
-                console.log(this.editor)
             }
         },
         computed: {
@@ -71,8 +101,5 @@
 </script>
 
 <style >
-    .ql-editor{
-        min-height: 300px;
-    }
 
 </style>
