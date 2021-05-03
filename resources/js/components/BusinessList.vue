760a02e1524b64ab5082e58fbe3f65b7ad645aac
waiyan <waiyanavionics@gmail.com>
2021-05-03 11:46:52 +0630

    fixed layout

--- resources/js/components/BusinessList.vue
+++ resources/js/components/BusinessList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="q-pa-md" style="max-width: 350px">
+    <div class="q-pa-md">
         <q-list @click="alert = true">
             <q-item>
                 <q-item-section>
--- resources/views/home_pc.blade.php
+++ resources/views/home_pc.blade.php
@@ -1,20 +1,18 @@
-<div class="tw-container tw-mx-auto tw-px-4">
-    <div class="tw-flex tw-py-6">
-        {{-- bussiness --}}
-        <div class="tw-w-1/4">
-            <business-list></business-list>
-        </div>
-        {{-- news feed --}}
-        <div class="tw-w-2/4">
-            <div class="tw-flex tw-justify-center">
-                <div class="">
-                    @include('posts')
-                </div>
+<div class="tw-flex tw-py-6">
+    {{-- bussiness --}}
+    <div class="tw-w-1/4 tw-ml-10">
+        <business-list></business-list>
+    </div>
+    {{-- news feed --}}
+    <div class="tw-w-2/4">
+        <div class="tw-flex tw-justify-center">
+            <div class="">
+                @include('posts')
             </div>
         </div>
-        {{-- advertisements --}}
-        <div class="tw-w-1/4">
-            <advertisement-list></advertisement-list>
-        </div>
+    </div>
+    {{-- advertisements --}}
+    <div class="tw-w-1/4">
+        <advertisement-list></advertisement-list>
     </div>
 </div>
