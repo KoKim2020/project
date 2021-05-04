8298f63d4e5badbe7379a616dac7f1e979a4ad98
waiyan <waiyanavionics@gmail.com>
2021-05-04 20:36:02 +0630

    tested

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -144,7 +144,7 @@ class PostController extends Controller
      */
     public function getCategory()
     {
-        return Category::all();
+        return Category::all(); //
     }
 
     // get posts of selected categories
