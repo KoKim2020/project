54e17dc385e37f586712f54a9144902cc74deb34
waiyan <waiyanavionics@gmail.com>
2021-05-06 14:40:59 +0630

    tabs added

--- resources/js/components/AuthorProfile.vue
+++ resources/js/components/AuthorProfile.vue
@@ -4,29 +4,57 @@
         <q-card class="my-card" flat bordered>
             <q-item>
                 <q-item-section avatar>
-                    <q-avatar square size="150px">
-                        <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+                    <q-avatar square size="135px">
+                        <img :src="$user.profile_img" />
                     </q-avatar>
                 </q-item-section>
 
                 <q-item-section>
-                    <q-item-label>Title</q-item-label>
-                    <q-item-label caption> Subhead </q-item-label>
+                    <div class="text-h6">
+                        {{ $user.name }}
+                    </div>
+                    <div class="text-subtitle1">
+                        Server Admin
+                    </div>
                 </q-item-section>
+
             </q-item>
 
             <q-separator />
 
-            <q-card-section horizontal>
-                <q-card-section>
-                    {{ lorem }}
-                </q-card-section>
+            <q-card-section>
+                <q-tabs
+                    v-model="tab"
+                    dense
+                    class="text-grey"
+                    active-color="primary"
+                    indicator-color="primary"
+                    align="justify"
+                    narrow-indicator
+                >
+                    <q-tab name="mails" label="Mails" />
+                    <q-tab name="alarms" label="Alarms" />
+                    <q-tab name="movies" label="Movies" />
+                </q-tabs>
 
-                <q-separator vertical />
+                <q-separator />
 
-                <q-card-section class="col-4">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </q-card-section>
+                <q-tab-panels v-model="tab" animated>
+                    <q-tab-panel name="mails">
+                        <div class="text-h6">Mails</div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </q-tab-panel>
+
+                    <q-tab-panel name="alarms">
+                        <div class="text-h6">Alarms</div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </q-tab-panel>
+
+                    <q-tab-panel name="movies">
+                        <div class="text-h6">Movies</div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </q-tab-panel>
+                </q-tab-panels>
             </q-card-section>
         </q-card>
     </div>
@@ -37,12 +65,9 @@
         data () {
             return {
                 lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-                // testing: window.User
+                tab: 'mails'
             }
         },
-        // created () {
-        //     console.log(window.User)
-        // }
     }
 </script>
 
