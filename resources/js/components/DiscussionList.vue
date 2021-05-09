0794fa7e13c36359c52108dadb935825c73f0012
waiyan <waiyanavionics@gmail.com>
2021-05-09 09:13:59 +0630

    remove unnecessary things

--- resources/js/components/DiscussionList.vue
+++ resources/js/components/DiscussionList.vue
@@ -30,33 +30,7 @@
             </template>
         </q-input>
         
-        <q-list bordered padding>
-            <q-item>
-                <q-item-section>
-                    <q-item-label overline>OVERLINE</q-item-label>
-                    <q-item-label>Single line item</q-item-label>
-                    <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
-                </q-item-section>
-
-                <q-item-section side top>
-                    <q-item-label caption>5 min ago</q-item-label>
-                </q-item-section>
-            </q-item>
-
-            <q-separator spaced />
-            <q-item-label header>List Header</q-item-label>
-
-            <q-item>
-                <q-item-section avatar>
-                    <q-icon color="primary" name="bluetooth" />
-                </q-item-section>
-                <q-item-section>List item</q-item-section>
-                <q-item-section side>
-                    <q-item-label caption>meta</q-item-label>
-                </q-item-section>
-            </q-item>
-
-            <q-separator spaced inset="item" />
+        <q-list padding>
 
             <q-item>
                 <q-item-section top avatar>
@@ -70,7 +44,6 @@
 
                 <q-item-section side top>
                 <q-item-label caption>5 min ago</q-item-label>
-                <q-icon name="star" color="yellow" />
                 </q-item-section>
             </q-item>
 
