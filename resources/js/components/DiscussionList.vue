6c5399047491a299d251a5c234569a4eeb269ff1
waiyan <waiyanavionics@gmail.com>
2021-05-09 09:36:32 +0630

    remove unnecessary things

--- resources/js/components/DiscussionList.vue
+++ resources/js/components/DiscussionList.vue
@@ -37,7 +37,9 @@
 
             <q-item>
                 <q-item-section top avatar>
-                    <q-avatar color="primary" text-color="white" icon="bluetooth" />
+                    <q-avatar>
+                        <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+                    </q-avatar>
                 </q-item-section>
 
                 <q-item-section>
@@ -53,79 +55,8 @@
             <q-item>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro voluptas quam harum reiciendis dignissimos dolorem sequi optio aliquam ullam. Accusantium illo dolorum aspernatur non voluptas possimus facere temporibus debitis vero?</p>
             </q-item>
-
-            <q-separator spaced inset="item" />
-
-            <q-item>
-                <q-item-section top avatar>
-                    <q-avatar color="primary" text-color="white" square icon="bluetooth" />
-                </q-item-section>
-
-                <q-item-section>
-                    <q-item-label>Single line item</q-item-label>
-                    <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
-                </q-item-section>
-
-                <q-item-section side top>
-                    <q-item-label caption>meta</q-item-label>
-                </q-item-section>
-                \
-            </q-item>
-
-            <q-separator spaced inset="item" />
-
-            <q-item>
-                <q-item-section top avatar>
-                    <q-avatar>
-                        <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-                    </q-avatar>
-                </q-item-section>
-
-                <q-item-section>
-                    <q-item-label>Single line item</q-item-label>
-                    <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
-                </q-item-section>
-
-                <q-item-section side top>
-                    <q-badge label="10k" />
-                </q-item-section>
-            </q-item>
-
-            <q-separator spaced inset="item" />
-
-            <q-item>
-                <q-item-section top avatar>
-                    <q-avatar rounded>
-                        <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-                    </q-avatar>
-                </q-item-section>
-
-                <q-item-section>
-                    <q-item-label>Single line item</q-item-label>
-                    <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
-                </q-item-section>
-
-                <q-item-section side top>
-                    <q-item-label caption>meta</q-item-label>
-                </q-item-section>
-            </q-item>
-
-            <q-separator spaced />
-
-            <q-item>
-                <q-item-section top thumbnail class="q-ml-none">
-                    <img src="https://cdn.quasar.dev/img/mountains.jpg">
-                </q-item-section>
-
-                <q-item-section>
-                    <q-item-label>Single line item</q-item-label>
-                    <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
-                </q-item-section>
-
-                <q-item-section side top>
-                    <q-item-label caption>meta</q-item-label>
-                </q-item-section>
-            </q-item>
+            <q-separator spaced  />
+            
         </q-list>
         <q-dialog v-model="alert">
             <q-card  style="width: 350px; max-width: 80vw;">
